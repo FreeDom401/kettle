f1565856bff3063bca4af369ef952a38007c207f
hangpengfei <13572498633@163.com>
2019-07-09 15:00:29 +0800

    dashuju

---
+++ src/test/java/net/oschina/dev/test/StrTest.java
@@ -0,0 +1,31 @@
+/**
+* Project Name:dev-tp
+* Date:2016年8月5日
+* Copyright (c) 2016, jingma All Rights Reserved.
+*/
+
+package net.oschina.dev.test;
+
+import com.eova.common.utils.EncryptUtil;
+import org.junit.Test;
+
+/**
+ * 字符串测试 <br/>
+ * date: 2016年8月5日 <br/>
+ * @author jingma
+ * @version 
+ */
+public class StrTest {
+    @Test
+    public void splitTest(){
+        System.out.println("dsad-232".split("-").length);
+        System.out.println("dsad-".split("-").length);
+        System.out.println("dsad".split("-").length);
+    }
+
+    @Test
+    public void testEnc() {
+        String encStr = EncryptUtil.getSM32("admin");
+        System.out.println(encStr);
+    }
+}
